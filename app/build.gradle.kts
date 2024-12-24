82007b3147a306db1f2bc481a46222b8abaf0964
Riedho-Syafei <109266989+Riedho-Syafei@users.noreply.github.com>
2024-12-24 10:21:02 +0700

    Buat Bima

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,8 +42,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation("com.google.android.gms:play-services-maps:19.0.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    implementation("com.android.volley:volley:1.2.1")
 }
